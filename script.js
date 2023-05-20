a5d5029e4baf0b244bbdb5d3396618b0e6c8df43
bkrmroy <110381880+bkrmroy@users.noreply.github.com>
2023-05-20 14:09:35 +0600

    add hover effect

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
 const container = document.querySelector(".container");
+console.log(container.clientWidth)
+
 
 function createGrid(rowNum, columnNum){
     for (r = 0; r < rowNum; r++) {
         const row = document.createElement("div");
-        row.setAttribute("style","display : flex;");
+        row.setAttribute("style","display : flex; flex-shrink: 0;");
 
         for(c = 0; c < columnNum; c++){
             const column = document.createElement("div");
             row.appendChild(column).className = "gridColumn";
         }
         container.appendChild(row).className = "gridRow";
-    };
+    }
 }
-createGrid(16,16);
+createGrid(16, 16);
+
+const singleGrid = document.getElementsByClassName("gridColumn");
+
+
+for(i=0; i < 256; i++ ){
+
+    singleGrid[i].addEventListener('mousedown', function(){
+        this.style.backgroundColor =  'black';
+    });
+}
+
+
 
