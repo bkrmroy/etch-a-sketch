57f252e983e468914d40964f06b2355be7f8757a
bkrmroy <110381880+bkrmroy@users.noreply.github.com>
2023-05-21 17:47:21 +0600

    add black and random color button
    clicking on the black button allows user to draw with black color
    clicking on the random color button allows user to draw with random color

--- script.js
+++ script.js
@@ -25,31 +25,72 @@ let columnNum = rowNum;
 createGrid(rowNum, columnNum);
 
 //draw when clicked on cell
+
 const cell = document.getElementsByClassName("cell");
-function draw(){
+const btnBlack = document.querySelector('.black');
+const btnRandomColor = document.querySelector('.random-color');
+
+function drawBlack(){
     for(i=0; i < rowNum * columnNum; i++ ){
         cell[i].addEventListener('mouseover', function(){
             this.style.backgroundColor =  'black';
         });
     }
 }
-draw();
+let randomColor;
+function generateRandomColor() {
+    let x = Math.floor(Math.random() * 256);
+    let y = Math.floor(Math.random() * 256);
+    let z = Math.floor(Math.random() * 256);
+    randomColor = `rgb(${x},${y},${z})`;
+    return randomColor;
+    }
+
+function drawRandom(){
+    for(i=0; i < rowNum * columnNum; i++ ){
+        cell[i].addEventListener('mouseover', function(){
+            generateRandomColor();
+            this.style.backgroundColor =  randomColor;
+        });
+    }
+    
+}
+
+btnBlack.addEventListener('click',function(){
+    
+    btnBlack.setAttribute('style','background-color: #f78f5c; transform: scale(0.95);box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);');
+    btnRandomColor.setAttribute('style','');
+    btnBlack.classList.add('blackActive');
+    if(btnBlack.classList.contains('blackActive')){
+        drawBlack();
+    }
+})
+btnRandomColor.addEventListener('click',function(){
+    btnRandomColor.setAttribute('style','background-color: #f78f5c; transform: scale(0.95);box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);');
+    btnBlack.setAttribute('style','');
+    btnRandomColor.classList.add('randomActive');
+    if(btnRandomColor.classList.contains('randomActive')){
+        drawRandom();
+    }
+})
+
+
+
 
 
 //reset grid or change grid size
 const reset = document.querySelector(".reset");
 reset.addEventListener('click',function(){
     const x = parseInt(prompt("How many squares per side? (between 0 and 100)"));
-    console.log(x);
-    console.log(typeof(x));
     rowNum = x ;
     columnNum = rowNum;
+    btnBlack.setAttribute('style','');
+    btnRandomColor.setAttribute('style','');
     if(x>0 && x<=100){
         while (container.firstChild) {
             container.removeChild(container.firstChild);
           }
         createGrid(rowNum, columnNum);
-        draw();
     }
 })
 
